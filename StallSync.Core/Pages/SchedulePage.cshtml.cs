4c9490c4850490c77740e8ec4e2e94a64ae75827
Cassandra <cassandra.lidman@outlook.com>
2024-12-03 11:35:06 +0100

    feature download schedule csv

---
+++ StallSync.Core/Pages/SchedulePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StallSync.Pages
+{
+    public class SchedulePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
