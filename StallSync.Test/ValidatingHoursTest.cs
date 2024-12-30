ae20c39c83108ee4b6bca017cc48763c4a31065b
Anton Engstrom <anton.engstrom1994@gmail.com>
2024-12-30 21:33:32 +0100

    Detta äar val implementerat?

--- StallSync.Test/ValidatingHoursTest.cs
+++ StallSync.Test/ValidatingHoursTest.cs
@@ -6,27 +6,29 @@ using Xunit;
 public class ValidatingHoursTest
 {
     [Theory]
-    [InlineData("2024-12-05T06:00", true)] 
-    [InlineData("2024-12-05T19:00", true)] 
-    [InlineData("2024-12-05T05:59", false)]
-    [InlineData("2024-12-05T20:01", false)] 
-    [InlineData("2024-12-05T15:30", true)]
-    [InlineData("2024-12-05T00:00", false)]
-    [InlineData("2024-12-06T06:00", true)]
-    [InlineData("2024-12-05T20:00", false)]
+    [InlineData("2025-12-05T06:00", true)] 
+    [InlineData("2025-12-05T19:00", true)] 
+    [InlineData("2025-12-05T05:59", false)]
+    [InlineData("2025-12-05T20:01", false)] 
+    [InlineData("2025-12-05T15:30", true)]
+    [InlineData("2025-12-05T00:00", false)]
+    [InlineData("2025-12-06T06:00", true)]
+    [InlineData("2025-12-05T20:00", false)]
     public void ValidateStartDate_WithinAllowedRange_ReturnsExpectedResult(string startDateString, bool expected)
     {
         // Arrange
         var startDate = DateTime.Parse(startDateString);
         var sut = new TaskItem { StartDate = startDate };
+        var validationContext = new ValidationContext(sut)
+        {
+            MemberName = nameof(TaskItem.StartDate)
+        };
 
         // Act
-        var isValid =
-            sut.StartDate.TimeOfDay >= TimeSpan.FromHours(6) && 
-            sut.StartDate.TimeOfDay <= TimeSpan.FromHours(19).Add(TimeSpan.FromMinutes(59));
+        var validResult = new List<ValidationResult>();
+        var isValid = Validator.TryValidateProperty(sut.StartDate, validationContext, validResult);
 
         // Assert
         Assert.Equal(expected, isValid);
-        
     }
 }
